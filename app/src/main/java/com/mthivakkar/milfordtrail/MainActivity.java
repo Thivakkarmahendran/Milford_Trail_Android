a9ca5989a2d3e7bc24cdbcd3e549a650e6a444a7
Thivakkar Mahendran <mthivakkar@gmail.com>
2019-07-26 16:36:46 -0400

    added konkait sdk

--- app/src/main/java/com/mthivakkar/milfordtrail/MainActivity.java
+++ app/src/main/java/com/mthivakkar/milfordtrail/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kontakt.sdk.android.common.KontaktSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        KontaktSDK.initialize("uONVvSjhWhXtcRiHMqaQHzxMQBRXBnRK");
     }
 
 
